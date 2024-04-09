6539323d85b658fdd5f9ee238db1441dacec24d6
Hafiza-Jabeen-Iqbal <163616796+Hafiza-Jabeen-Iqbal@users.noreply.github.com>
2024-04-09 05:44:50 +0500

    Update 11_Array.js

--- JS/11_Array.js
+++ JS/11_Array.js
@@ -55,4 +55,19 @@ shallow copy     vs      Deep copy
 // console.log(result);
 
 
+//Array map method
+
+let users = [
+  {firstName : "Susan", lastName: "Steward"},
+  {firstName : "Daniel", lastName: "Longbottom"},
+  {firstName : "Jacob", lastName: "Black"}
+];
+
+let userFullnames = users.map(function(element){
+    return `${element.firstName} ${element.lastName}`;
+})
+
+console.log(userFullnames);
+// ["Susan Steward", "Daniel Longbottom", "Jacob Black"]
+
 
